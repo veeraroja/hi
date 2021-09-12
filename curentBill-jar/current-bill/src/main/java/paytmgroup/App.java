77abd0175b5d6785d7f012bc381b3ebed998bb4c
veeraroja <veera1619@gmail.com>
2021-09-12 10:53:16 +0530

    new code modified

--- curentBill-jar/current-bill/src/main/java/paytmgroup/App.java
+++ curentBill-jar/current-bill/src/main/java/paytmgroup/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hi current-bill in paytm" );
+        System.out.println( "Hi current-bill in paytm project" );
     }
 }
